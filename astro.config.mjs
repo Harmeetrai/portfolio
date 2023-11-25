507e6c3bba52d47555cb859d3ea93c11eaa58432
Harmeetrai <harmeetrai18@gmail.com>
2023-11-24 21:34:22 -0800

    add tailwind config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,48 @@
-import { defineConfig } from 'astro/config';
-
+import { defineConfig } from "astro/config";
+import fs from "fs";
+import mdx from "@astrojs/mdx";
+import tailwind from "@astrojs/tailwind";
+import sitemap from "@astrojs/sitemap";
+import prefetch from "@astrojs/prefetch";
+import remarkUnwrapImages from "remark-unwrap-images";
+import remarkReadingTime from "./src/utils/remark-reading-time.ts"
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+	// ! Please remember to replace the following site property with your own domain
+	site: "https://astro-cactus.chriswilliams.dev/",
+
+	integrations: [
+		mdx({}),
+		tailwind({
+			applyBaseStyles: false,
+		}),
+		sitemap(),
+		prefetch(),
+	],
+	image: {
+		domains: ["webmention.io"],
+	},
+	vite: {
+		plugins: [rawFonts([".ttf"])],
+		optimizeDeps: {
+			exclude: ["@resvg/resvg-js"],
+		},
+	},
+});
+
+function rawFonts(ext) {
+	return {
+		name: "vite-plugin-raw-fonts",
+		// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+		// @ts-ignore:next-line
+		transform(_, id) {
+			if (ext.some((e) => id.endsWith(e))) {
+				const buffer = fs.readFileSync(id);
+				return {
+					code: `export default ${JSON.stringify(buffer)}`,
+					map: null,
+				};
+			}
+		},
+	};
+}
